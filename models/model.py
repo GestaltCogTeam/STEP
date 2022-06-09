9ba5252d3784ae647cac664729e786c26b249064
Zezhi Shao <864453277@qq.com>
2022-06-09 15:19:26 +0800

    typos

--- models/model.py
+++ models/model.py
@@ -18,26 +18,26 @@ def gumbel_softmax_sample(logits, temperature, eps=1e-10):
     return F.softmax(y / temperature, dim=-1)
 
 def gumbel_softmax(logits, temperature, hard=False, eps=1e-10):
-  """Sample from the Gumbel-Softmax distribution and optionally discretize.
-  Args:
-    logits: [batch_size, n_class] unnormalized log-probs
-    temperature: non-negative scalar
-    hard: if True, take argmax, but differentiate w.r.t. soft sample y
-  Returns:
-    [batch_size, n_class] sample from the Gumbel-Softmax distribution.
-    If hard=True, then the returned sample will be one-hot, otherwise it will
-    be a probabilitiy distribution that sums to 1 across classes
-  """
-  y_soft = gumbel_softmax_sample(logits, temperature=temperature, eps=eps)
-  if hard:
-      shape = logits.size()
-      _, k = y_soft.data.max(-1)
-      y_hard = torch.zeros(*shape).to(logits.device)
-      y_hard = y_hard.zero_().scatter_(-1, k.view(shape[:-1] + (1,)), 1.0)
-      y = torch.autograd.Variable(y_hard - y_soft.data) + y_soft
-  else:
-      y = y_soft
-  return y
+    """Sample from the Gumbel-Softmax distribution and optionally discretize.
+    Args:
+        logits: [batch_size, n_class] unnormalized log-probs
+        temperature: non-negative scalar
+        hard: if True, take argmax, but differentiate w.r.t. soft sample y
+    Returns:
+        [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+        If hard=True, then the returned sample will be one-hot, otherwise it will
+        be a probabilitiy distribution that sums to 1 across classes
+    """
+    y_soft = gumbel_softmax_sample(logits, temperature=temperature, eps=eps)
+    if hard:
+        shape = logits.size()
+        _, k = y_soft.data.max(-1)
+        y_hard = torch.zeros(*shape).to(logits.device)
+        y_hard = y_hard.zero_().scatter_(-1, k.view(shape[:-1] + (1,)), 1.0)
+        y = torch.autograd.Variable(y_hard - y_soft.data) + y_soft
+    else:
+        y = y_soft
+    return y
 
 class STGNN(nn.Module):
     def __init__(self, cfg, **model_args):
@@ -80,8 +80,7 @@ class STGNN(nn.Module):
             classes = set(labels)
             classes_dict = {c: np.identity(len(classes))[i, :] for i, c in
                             enumerate(classes)}
-            labels_onehot = np.array(list(map(classes_dict.get, labels)),
-                                     dtype=np.int32)
+            labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
             return labels_onehot
         # Generate off-diagonal interaction graph
         if self.dataset_name == 'METR-LA':
