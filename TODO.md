f5ee3051f32dc9b1f5da50d3fde0faf76e1fe5ed
Zezhi Shao <864453277@qq.com>
2023-07-03 00:52:08 +0800

    add PEMS08

--- TODO.md
+++
@@ -1,5 +0,0 @@
-# TODO List
-
-- (Very Soon) Add configurations for PEMS-BAY and PEMS04.
-- (Very Soon) Add training logs and checkpoints for all datasets.
-- Update README.md and datasets/README.md
--- step/run.py
+++ step/run.py
@@ -4,20 +4,27 @@ from argparse import ArgumentParser
 
 # TODO: remove it when basicts can be installed by pip
 sys.path.append(os.path.abspath(__file__ + "/../.."))
+import torch
 from basicts import launch_training
+
+torch.set_num_threads(2) # aviod high cpu avg usage
 
 
 def parse_args():
     parser = ArgumentParser(description="Run time series forecasting model in BasicTS framework!")
     # parser.add_argument("-c", "--cfg", default="step/TSFormer_METR-LA.py", help="training config")
-    # parser.add_argument("-c", "--cfg", default="step/STEP_METR-LA.py", help="training config")
+    # parser.add_argument("-c", "--cfg", default="step/÷STEP_METR-LA.py", help="training config")
 
     # parser.add_argument("-c", "--cfg", default="step/TSFormer_PEMS04.py", help="training config")
-    parser.add_argument("-c", "--cfg", default="step/STEP_PEMS04.py", help="training config")
+    # parser.add_argument("-c", "--cfg", default="step/STEP_PEMS04.py", help="training config")
 
     # parser.add_argument("-c", "--cfg", default="step/TSFormer_PEMS-BAY.py", help="training config")
     # parser.add_argument("-c", "--cfg", default="step/STEP_PEMS-BAY.py", help="training config")
-    parser.add_argument("--gpus", default="0, 1", help="visible gpus")
+
+    # parser.add_argument("-c", "--cfg", default="step/TSFormer_PEMS08.py", help="training config")
+    parser.add_argument("-c", "--cfg", default="step/STEP_PEMS08.py", help="training config")
+    
+    parser.add_argument("--gpus", default="0", help="visible gpus")
     return parser.parse_args()
 
 if __name__ == "__main__":
